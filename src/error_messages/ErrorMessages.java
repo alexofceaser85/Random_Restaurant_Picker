59257b8a79713304c96cbf6b9ebb2d9b5b4121cf
alex decesare <adecesa1@my.westga.edu>
2020-11-18 15:17:42 -0500

    Added the error messages class
    Added the error messages class to hold the error messages for the
    application. Also added the error messages for the Restaurant class.

---
+++ src/error_messages/ErrorMessages.java
@@ -0,0 +1,25 @@
+package error_messages;
+
+/**
+ * The class for the error messages
+ * 
+ * @author Alex DeCesare
+ * @version 18-November-2020
+ *
+ */
+
+public class ErrorMessages {
+
+	public static final String RESTAURANT_NAME_SHOULD_NOT_BE_NULL = "The restaurant's name cannot be null";
+	public static final String RESTAURANT_NAME_SHOULD_NOT_BE_EMPTY = "The restaurant's name cannot be empty";
+	public static final String RESTAURANT_LOCATION_SHOULD_NOT_BE_NULL = "The restaurant's location cannot be null";
+	public static final String RESTAURANT_LOCATION_SHOULD_NOT_BE_EMPTY = "The restaurant's location cannot be empty";
+	public static final String RESTAURANT_DISTANCE_SHOULD_NOT_BE_LESS_THAN_ZERO = "The restaurant's distance cannot be less than zero";
+	public static final String RESTAURANT_REVIEW_SCORE_SHOULD_NOT_BE_LESS_THAN_ZERO = "The restaurant's review score cannot be less than zero";
+	public static final String RESTAURANT_MENU_URL_SHOULD_NOT_BE_NULL = "The restaurant's menu URL cannot be null";
+	public static final String RESTAURANT_MENU_URL_SHOULD_NOT_BE_EMPTY = "The restaurant's menu URL cannot be empty";
+	public static final String RESTAURANT_IMAGE_URL_SHOULD_NOT_BE_NULL = "The restaurant's image URL cannot be null";
+	public static final String RESTAURANT_IMAGE_URL_SHOULD_NOT_BE_EMPTY = "The restaurant's image URL cannot be empty";
+	public static final String RESTAURANT_ID_SHOULD_NOT_NULL = "The restaurant's id cannot be null";
+	public static final String RESTAURANT_ID_SHOULD_NOT_EMPTY = "The restaurant's id cannot be empty";
+}
