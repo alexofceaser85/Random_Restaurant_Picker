f409bc962709d7669b59c7058ac8f65936a558b2
Alexander Ayers <alexayers2000@gmail.com>
2020-11-19 20:14:21 -0500

    Testing Price Enum
    Added testing for the price enum to ensure that the constant equals the
    string version.

---
+++ src/test/price/TestConstructor.java
@@ -0,0 +1,19 @@
+package test.price;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import model.Price;
+
+class TestConstructor {
+
+	@Test
+	void testValueEqualsStringOutput() {
+		Price actual = Price.$$;
+		
+		String expected = "$$";
+		assertEquals(expected, actual.toString());
+	}
+
+}
