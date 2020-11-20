a62d5930ff3d4489f1f48978f0ce7b493c23dcd3
Alexander Ayers <alexayers2000@gmail.com>
2020-11-19 21:56:50 -0500

    Made RestaurantViewCodeBehind class
    Made the template code behind class for the Restaurant View. Needs to be
    fully implemented with details and viewModel linking.

---
+++ src/view/RestaurantViewCodeBehind.java
@@ -0,0 +1,67 @@
+package view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.text.Text;
+
+/**
+ * Code Behind for the Restaurant View
+ * @author Alexander Ayers
+ *
+ */
+
+public class RestaurantViewCodeBehind {
+
+    @FXML
+    private Text restaurantNameText;
+
+    @FXML
+    private Text restaurantDistanceText;
+
+    @FXML
+    private Hyperlink restaurantAddressHyperlink;
+
+    @FXML
+    private Text restaurantPriceText;
+
+    @FXML
+    private Text restaurantReivewScoreText;
+
+    @FXML
+    private Button seeRestauarntReviewsButton;
+
+    @FXML
+    private Button seeRestauarntMenuButton;
+
+    @FXML
+    private Button grabNewRestaurantButton;
+
+    @FXML
+    private Button resetFiltersButton;
+    
+    //private RestaurantViewViewModel viewModel;
+
+    @FXML
+    void grabNewRestaurant(ActionEvent event) {
+
+    }
+
+    @FXML
+    void resetFilters(ActionEvent event) {
+
+    }
+
+    @FXML
+    void seeRestaurantButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void seeRestaurantReviews(ActionEvent event) {
+
+    }
+
+}
+
