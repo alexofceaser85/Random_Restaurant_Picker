8e30ea601e33074c80c874ea3f48fd5159036f60
Alexander Ayers <alexayers2000@gmail.com>
2020-11-21 14:05:34 -0500

    Finished RestaurantViewCodeBehind
    Added all bindings for the RestaurantViewCodeBehind to properties in the
    viewmodel. Additionally, added in method calls to the viewModel for
    grabNewRestaurant() and resetFilters(). Menu Linking and
    RestaurantReviews buttons need to be implemented.

--- src/view/RestaurantViewCodeBehind.java
+++ src/view/RestaurantViewCodeBehind.java
@@ -1,5 +1,6 @@
 package src.view;
 
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -34,7 +35,7 @@ public class RestaurantViewCodeBehind {
     private Button seeRestaurantReviewsButton;
 
     @FXML
-    private Button seeRestauarntMenuButton;
+    private Button seeRestauarantMenuButton;
 
     @FXML
     private Button grabNewRestaurantButton;
@@ -43,25 +44,51 @@ public class RestaurantViewCodeBehind {
     private Button resetFiltersButton;
     
     private RestaurantViewModel viewModel;
-
+    
+    
+	/**
+	 * Zero-parameter constructor
+	 * 
+	 * @precondition none
+	 * @postcondition none
+	 */
+    public RestaurantViewCodeBehind() {
+    	this.viewModel = new RestaurantViewModel();
+    }
+    
+    
+	/**
+	 * Initializes all properties to needed settings.
+	 * 
+	 * @precondition none
+	 * @postcondition none
+	 */
+    public void initialize() {
+    	this.restaurantNameText.textProperty().bind(this.viewModel.nameProperty());
+    	this.restaurantDistanceText.textProperty().bind(this.viewModel.distanceProperty());
+    	this.restaurantAddressHyperlink.textProperty().bind(this.viewModel.locationProperty());
+    	this.restaurantPriceText.textProperty().bind(this.viewModel.priceRangeProperty());
+    	this.restaurantReivewScoreText.textProperty().bind(this.viewModel.reviewScoreProperty());
+    }
+    
     @FXML
     void grabNewRestaurant(ActionEvent event) {
-
+    	this.viewModel.pickARestaurant();
     }
 
     @FXML
     void resetFilters(ActionEvent event) {
-
+    	this.viewModel.resetFilters();
     }
 
     @FXML
-    void seeRestaurantButton(ActionEvent event) {
-
+    void seeRestaurantMenu(ActionEvent event) {
+    	throw new UnsupportedOperationException();
     }
 
     @FXML
     void seeRestaurantReviews(ActionEvent event) {
-
+    	throw new UnsupportedOperationException();
     }
 
 }
