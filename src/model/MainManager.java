ad075e938c5f4c507e8fd75dbad264edb51fd1e5
Alexander Ayers <alexayers2000@gmail.com>
2020-11-23 17:26:28 -0500

    Added Bevvy of Classes and FXML to View:
    Added FXML and CodeBehind classes for several pages within the GUI for
    future editing.

--- src/model/MainManager.java
+++ src/model/MainManager.java
@@ -5,19 +5,20 @@ import java.util.Random;
 public class MainManager {
 	private RestaurantManager restaurantManager;
 	private boolean resetFilters;
+
 	public MainManager(Random randomGenerator) {
 		this.resetFilters = false;
 		this.restaurantManager = new RestaurantManager(randomGenerator);
 	}
-	
+
 	public RestaurantManager getRestaurantManager() {
 		return this.restaurantManager;
 	}
-	
+
 	public boolean getResetFilters() {
 		return this.resetFilters;
 	}
-	
+
 	public void setResetFilters(boolean resetFilters) {
 		this.resetFilters = resetFilters;
 	}
