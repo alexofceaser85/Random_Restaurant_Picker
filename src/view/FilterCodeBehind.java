f6c22f34a96874b00e5f6b1a6793d2f597b31793
Alexander Ayers <alexayers2000@gmail.com>
2020-11-26 01:39:52 -0500

    Finished FXML for Filter
    Finalized the FXML for the FilterPageGUI and Begun the FilterCodeBehind.

--- src/view/FilterCodeBehind.java
+++ src/view/FilterCodeBehind.java
@@ -1,5 +1,90 @@
 package src.view;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
 public class FilterCodeBehind {
 
-}
+	  @FXML
+	    private Text enterLocationText;
+
+	    @FXML
+	    private TextField locationAddressTextBox;
+
+	    @FXML
+	    private Text enterRadiusText;
+
+	    @FXML
+	    private ComboBox<?> radiusComboBox;
+
+	    @FXML
+	    private Button beginButton;
+	    
+	    @FXML
+	    private Text categoriesText;
+
+	    @FXML
+	    private TextField categoriesTextBox;
+
+	    @FXML
+	    private TextField reviewScoreTextBox;
+
+	    @FXML
+	    private CheckBox price1Checkbox;
+
+	    @FXML
+	    private CheckBox price2Checkbox;
+
+	    @FXML
+	    private CheckBox price4Checkbox;
+
+	    @FXML
+	    private CheckBox price3Checkbox;
+
+	    @FXML
+	    private CheckBox currentOpenCheckbox;
+
+	    @FXML
+	    private CheckBox handicapAccessibleCheckbox;
+
+	    @FXML
+	    private CheckBox acceptsReservationsCheckbox;
+
+	    @FXML
+	    private CheckBox newRestaurantsCheckbox;
+
+	    @FXML
+	    private CheckBox genderNeutralBathroomsCheckbox;
+
+	    @FXML
+	    private Button backButton;
+
+	    @FXML
+	    private Button submitButton;
+	    
+	    public FilterCodeBehind() {
+	    	
+	    }
+
+	    @FXML
+	    void handleBegin(ActionEvent event) {
+
+	    }
+	    
+	    @FXML
+	    void handleBack(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void handleSubmit(ActionEvent event) {
+
+	    }
+
+	}
+
