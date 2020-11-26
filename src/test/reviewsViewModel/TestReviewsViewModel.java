0d73be682c3db8a6042e082468b260d15ddee239
alex decesare <adecesa1@my.westga.edu>
2020-11-26 11:43:01 -0500

    Added a toString to the reviews view model and its test code
    Added the toString method to the reviews view model to give a string
    representation of the class. I also added the test code for the reviews
    view model to ensure functionality for the class.

---
+++ src/test/reviewsViewModel/TestReviewsViewModel.java
@@ -0,0 +1,132 @@
+package src.test.reviewsViewModel;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import src.model.Review;
+import src.viewModel.ReviewsViewModel;
+
+class TestReviewsViewModel {
+
+	@Test
+	public void shouldNotAllowNullReview() {
+		
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			new ReviewsViewModel(null);
+		});
+	}
+	
+	@Test
+	public void shouldGetReviewer() {
+		
+		StringProperty reviewerNameTestAgainst = new SimpleStringProperty();
+		reviewerNameTestAgainst.set("Lewis Baumstark");
+		
+		Review theReview = new Review("Lewis Baumstark", "This lamb is so undercooked, it’s following Mary to school!", 3);
+		ReviewsViewModel theReviewsViewModel = new ReviewsViewModel(theReview);
+		
+		assertEquals(reviewerNameTestAgainst.getValue(), theReviewsViewModel.getReviewer().getValue());
+	}
+	
+	@Test
+	public void shouldGetReviewContent() {
+		
+		StringProperty reviewerContentTestAgainst = new SimpleStringProperty();
+		reviewerContentTestAgainst.set("This lamb is so undercooked, it’s following Mary to school!");
+		
+		Review theReview = new Review("Lewis Baumstark", "This lamb is so undercooked, it’s following Mary to school!", 3);
+		ReviewsViewModel theReviewsViewModel = new ReviewsViewModel(theReview);
+		
+		assertEquals(reviewerContentTestAgainst.getValue(), theReviewsViewModel.getContent().getValue());
+	}
+	
+	@Test
+	public void shouldGetReviewScoreAtMinimum() {
+		
+		DoubleProperty reviewScoreTestAgainst = new SimpleDoubleProperty();
+		reviewScoreTestAgainst.set(1);
+		
+		Review theReview = new Review("Lewis Baumstark", "This lamb is so undercooked, it’s following Mary to school!", 1);
+		ReviewsViewModel theReviewsViewModel = new ReviewsViewModel(theReview);
+		
+		assertEquals(reviewScoreTestAgainst.getValue(), theReviewsViewModel.getReviewScore().getValue());
+	}
+	
+	@Test
+	public void shouldGetReviewScoreOneTenthAboveMinimum() {
+		
+		DoubleProperty reviewScoreTestAgainst = new SimpleDoubleProperty();
+		reviewScoreTestAgainst.set(1.1);
+		
+		Review theReview = new Review("Lewis Baumstark", "This lamb is so undercooked, it’s following Mary to school!", 1.1);
+		ReviewsViewModel theReviewsViewModel = new ReviewsViewModel(theReview);
+		
+		assertEquals(reviewScoreTestAgainst.getValue(), theReviewsViewModel.getReviewScore().getValue());
+	}
+	
+	@Test
+	public void shouldGetReviewScoreOneAboveMinimum() {
+		
+		DoubleProperty reviewScoreTestAgainst = new SimpleDoubleProperty();
+		reviewScoreTestAgainst.set(2);
+		
+		Review theReview = new Review("Lewis Baumstark", "This lamb is so undercooked, it’s following Mary to school!", 2);
+		ReviewsViewModel theReviewsViewModel = new ReviewsViewModel(theReview);
+		
+		assertEquals(reviewScoreTestAgainst.getValue(), theReviewsViewModel.getReviewScore().getValue());
+	}
+	
+	@Test
+	public void shouldGetReviewScoreOneTenthBelowMaximum() {
+		
+		DoubleProperty reviewScoreTestAgainst = new SimpleDoubleProperty();
+		reviewScoreTestAgainst.set(4.9);
+		
+		Review theReview = new Review("Lewis Baumstark", "This lamb is so undercooked, it’s following Mary to school!", 4.9);
+		ReviewsViewModel theReviewsViewModel = new ReviewsViewModel(theReview);
+		
+		assertEquals(reviewScoreTestAgainst.getValue(), theReviewsViewModel.getReviewScore().getValue());
+	}
+	
+	@Test
+	public void shouldGetReviewScoreOneBelowMaximum() {
+		
+		DoubleProperty reviewScoreTestAgainst = new SimpleDoubleProperty();
+		reviewScoreTestAgainst.set(4);
+		
+		Review theReview = new Review("Lewis Baumstark", "This lamb is so undercooked, it’s following Mary to school!", 4);
+		ReviewsViewModel theReviewsViewModel = new ReviewsViewModel(theReview);
+		
+		assertEquals(reviewScoreTestAgainst.getValue(), theReviewsViewModel.getReviewScore().getValue());
+	}
+	
+	@Test
+	public void shouldGetReviewScoreAtMaximum() {
+		
+		DoubleProperty reviewScoreTestAgainst = new SimpleDoubleProperty();
+		reviewScoreTestAgainst.set(5);
+		
+		Review theReview = new Review("Lewis Baumstark", "This lamb is so undercooked, it’s following Mary to school!", 5);
+		ReviewsViewModel theReviewsViewModel = new ReviewsViewModel(theReview);
+		
+		assertEquals(reviewScoreTestAgainst.getValue(), theReviewsViewModel.getReviewScore().getValue());
+	}
+	
+	@Test
+	public void shouldReturnStringDescriptionOfViewModel() {
+		
+		StringProperty reviewerNameTestAgainst = new SimpleStringProperty();
+		reviewerNameTestAgainst.set("Reviewer: Lewis Baumstark Content: This lamb is so undercooked, it’s following Mary to school! Review Score: 4.0");
+		
+		Review theReview = new Review("Lewis Baumstark", "This lamb is so undercooked, it’s following Mary to school!", 4);
+		ReviewsViewModel theReviewsViewModel = new ReviewsViewModel(theReview);
+		
+		assertEquals(reviewerNameTestAgainst.getValue(), theReviewsViewModel.toString());
+	}
+}
--- src/viewModel/ReviewsViewModel.java
+++ src/viewModel/ReviewsViewModel.java
@@ -1,6 +1,8 @@
 package src.viewModel;
 
 import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import src.error_messages.ErrorMessages;
 import src.model.Review;
@@ -36,6 +38,10 @@ public class ReviewsViewModel {
 			throw new IllegalArgumentException(ErrorMessages.REVIEW_TO_GET_FOR_VIEW_MODEL_CANNOT_BE_NULL);
 		}
 		
+		this.reviewer = new SimpleStringProperty();
+		this.reviewContent = new SimpleStringProperty();
+		this.reviewScore = new SimpleDoubleProperty();
+
 		this.reviewer.set(review.getReviewerName());
 		this.reviewContent.set(review.getReviewContent());
 		this.reviewScore.set(review.getReviewScore());
@@ -80,4 +86,17 @@ public class ReviewsViewModel {
 		return this.reviewScore;
 	}
 	
+	/**
+	 * Returns the string representation of the reviews view model
+	 * 
+	 * @precondition none
+	 * @postcondition none
+	 * 
+	 * @return the string representation of the reviews view model
+	 */
+	
+	@Override
+	public String toString() {
+		return "Reviewer: " + this.getReviewer().getValue() + " Content: " + this.getContent().getValue() + " Review Score: " + this.getReviewScore().getValue();
+	}
 }
