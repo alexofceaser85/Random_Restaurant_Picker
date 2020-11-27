780ff62e4a54af6143cfac395808ff1fb4843484
Alexander Ayers <alexayers2000@gmail.com>
2020-11-26 19:32:27 -0500

    Finished Implementing CodeBehind
    Finalized the CodeBehind, only needs viewModel bindings(when viewModel
    is created), and linking to the SceneContrller when that is implemented.

--- src/view/FilterCodeBehind.java
+++ src/view/FilterCodeBehind.java
@@ -8,7 +8,11 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import src.model.Radius;
-
+/**
+ * Code Behind for the Filter Section of the GUI.
+ * @author Alexander Ayers
+ *
+ */
 public class FilterCodeBehind {
 
 	  @FXML
@@ -68,10 +72,27 @@ public class FilterCodeBehind {
 	    @FXML
 	    private Button submitButton;
 	    
+		/**
+		 * Zero-parameter constructor
+		 * 
+		 * @precondition none
+		 * @postcondition none
+		 */
 	    public FilterCodeBehind() {
 	    	
 	    }
-
+	    
+		/**
+		 * Initializes all properties to needed settings.
+		 * 
+		 * @precondition none
+		 * @postcondition none
+		 */
+	    public void initalize()
+	    {
+	    	this.beginButton.disableProperty().bind(this.locationAddressTextBox.textProperty().isEmpty());
+	    }
+	    
 	    @FXML
 	    void handleBegin(ActionEvent event) {
 
