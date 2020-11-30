35ccc0cabc81115ebb5542086dc5619c5a8dc6a1
alex decesare <adecesa1@my.westga.edu>
2020-11-29 22:03:10 -0500

    Added the descriptions for the main manager
    I added the descriptions for the main manager, such as class and method
    docstrings.

--- src/model/MainManager.java
+++ src/model/MainManager.java
@@ -3,19 +3,27 @@ package src.model;
 import java.util.Random;
 
 /**
- *
+ * The main manager for the application, holds the restaurant and review managers and the boolean to reset the filters
+ * 
  * @author johnc
  * @version Fall 2020
  */
+
 public class MainManager {
 	private RestaurantManager restaurantManager;
 	private ReviewManager reviewManager;
 	private boolean resetFilters;
 
 	/**
+	 * the constructor for the main manager
+	 * 
+	 * @param randomGenerator the random generater to get a random restaurant for
+	 * 
 	 * @precondition none
-	 * @postcondition none
-	 * @param randomGenerator
+	 * @postcondition 
+	 * 		this.resetFilters == false
+	 * 		this.restaurantManager == RestaurantManager(randomGenerator)
+	 * 		this.reviewManager == ReviewManager
 	 */
 	public MainManager(Random randomGenerator) {
 		this.resetFilters = false;
@@ -24,44 +32,48 @@ public class MainManager {
 	}
 
 	/**
+	 * The getter for the restaurant manager
 	 * 
 	 * @precondition none
-	 * @postconditon none
+	 * @postcondition none
 	 *
-	 * @return
+	 * @return the restaurant manager
 	 */
 	public RestaurantManager getRestaurantManager() {
 		return this.restaurantManager;
 	}
 
 	/**
+	 * the getter for the reset filters boolean
 	 * 
 	 * @precondition none
-	 * @postconditon none
+	 * @postcondition none
 	 *
-	 * @return
+	 * @return the reset filters boolean
 	 */
 	public boolean getResetFilters() {
 		return this.resetFilters;
 	}
 
 	/**
+	 * The setter for the reset filters boolean
 	 * 
 	 * @precondition none
-	 * @postconditon none
+	 * @postcondition this.resetFilters == resetFilters
 	 *
-	 * @param resetFilters
+	 * @param resetFilters the value to set the reset filters
 	 */
 	public void setResetFilters(boolean resetFilters) {
 		this.resetFilters = resetFilters;
 	}
 
 	/**
+	 * The getter for the review manager
 	 * 
 	 * @precondition none
 	 * @postconditon none
 	 *
-	 * @return
+	 * @return the review manager
 	 */
 	public ReviewManager getReviewManager() {
 		return this.reviewManager;
